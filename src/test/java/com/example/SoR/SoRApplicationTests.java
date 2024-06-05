3238b763a9346ac86974c557c860a2a2febd36a6
Arthur-ofk <arthauz19@gmail.com>
2024-06-05 15:43:06 +0300

    initial-c

---
+++ src/test/java/com/example/SoR/SoRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SoR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
